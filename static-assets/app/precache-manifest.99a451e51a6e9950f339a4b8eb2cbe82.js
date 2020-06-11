c1ec5941448413baf06bbe1965d41c1b71ce21a7
jvega190 <jose.vega@craftersoftware.com>
2020-06-11 10:00:19 -0600

    Update build

---
+++ static-assets/app/precache-manifest.99a451e51a6e9950f339a4b8eb2cbe82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b66a004bd58a25334f34fa39877730",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "3fd435ad58118a7daf73",
+    "url": "/static-assets/app/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "51b4af78842db53bd263",
+    "url": "/static-assets/app/static/css/4.57797527.chunk.css"
+  },
+  {
+    "revision": "d71b9b7c8d25a96cc014",
+    "url": "/static-assets/app/static/css/main.d5a51438.chunk.css"
+  },
+  {
+    "revision": "3fd435ad58118a7daf73",
+    "url": "/static-assets/app/static/js/2.a554292b.chunk.js"
+  },
+  {
+    "revision": "2c0e22c2f75a10fe48e1",
+    "url": "/static-assets/app/static/js/3.568b54bb.chunk.js"
+  },
+  {
+    "revision": "51b4af78842db53bd263",
+    "url": "/static-assets/app/static/js/4.ffd567ed.chunk.js"
+  },
+  {
+    "revision": "d71b9b7c8d25a96cc014",
+    "url": "/static-assets/app/static/js/main.5abbd449.chunk.js"
+  },
+  {
+    "revision": "e4b59a7f6003b8ab1240",
+    "url": "/static-assets/app/static/js/runtime~main.672692a5.js"
+  },
+  {
+    "revision": "cb50982a921f082c28b9b0f8e271fa62",
+    "url": "/static-assets/app/static/media/404.cb50982a.jpg"
+  },
+  {
+    "revision": "8282f1ec8e3598c74fedb340003736f8",
+    "url": "/static-assets/app/static/media/burger.8282f1ec.svg"
+  }
+]);
