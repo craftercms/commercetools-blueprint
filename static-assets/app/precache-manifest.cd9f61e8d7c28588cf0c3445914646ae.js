26b72a2842e1961d1f8b63c5f51918b93b1c8601
jvega190 <josevega19@gmail.com>
2022-06-23 12:17:45 -0600

    Build update

---
+++ static-assets/app/precache-manifest.cd9f61e8d7c28588cf0c3445914646ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0f667e6b6ca60579a3788e6bc003e0",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "39617a452890f4a0d312",
+    "url": "/static-assets/app/static/css/2.f6b9b443.chunk.css"
+  },
+  {
+    "revision": "72cf336c979fa3d97832",
+    "url": "/static-assets/app/static/css/4.80b3d6be.chunk.css"
+  },
+  {
+    "revision": "1d012a88f4de2bf7761b",
+    "url": "/static-assets/app/static/css/main.8ead40d3.chunk.css"
+  },
+  {
+    "revision": "39617a452890f4a0d312",
+    "url": "/static-assets/app/static/js/2.0e7461b2.chunk.js"
+  },
+  {
+    "revision": "0846a06cb92e9f9e6be98684f67798b2",
+    "url": "/static-assets/app/static/js/2.0e7461b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec79a019ce076ce2a540",
+    "url": "/static-assets/app/static/js/3.35bacf57.chunk.js"
+  },
+  {
+    "revision": "72cf336c979fa3d97832",
+    "url": "/static-assets/app/static/js/4.3a46068c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static-assets/app/static/js/4.3a46068c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d012a88f4de2bf7761b",
+    "url": "/static-assets/app/static/js/main.7e466512.chunk.js"
+  },
+  {
+    "revision": "1086606e60f03b71ef7e",
+    "url": "/static-assets/app/static/js/runtime-main.4684ca7e.js"
+  },
+  {
+    "revision": "cb50982a921f082c28b9b0f8e271fa62",
+    "url": "/static-assets/app/static/media/404.cb50982a.jpg"
+  },
+  {
+    "revision": "8282f1ec8e3598c74fedb340003736f8",
+    "url": "/static-assets/app/static/media/burger.8282f1ec.svg"
+  }
+]);
