8ae9d82c6f43cf110892adb0b8b03590a2cdcc15
jvega190 <josevega19@gmail.com>
2023-11-08 12:37:39 -0600

    Build update

--- sources/ecommerce-app/build.sh
+++ sources/ecommerce-app/build.sh
@@ -3,21 +3,15 @@
 echo "Making sure all deps are met"
 yarn
 
-echo "Running react build"
-react-scripts build
-
 echo "Deleting prior build"
 rm -rf ../../static-assets/app/*
+
+echo "Running react build"
+vite build
 
 echo "Restoring .gitkeep"
 touch ../../static-assets/app/.gitkeep
 
-echo "Copying app build to static assets"
-cp -rf ./build/* ../../static-assets/app/
-
 git add ../../static-assets/app
-
-echo "Delete react build output directory"
-rm -rf ./build
 
 echo "Build finished"
