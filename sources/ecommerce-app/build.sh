2b211903036a050db52ee72a77fec0687223425b
jvega190 <josevega19@gmail.com>
2023-11-17 15:19:18 -0600

    Move vite build command out from build.sh script

--- sources/ecommerce-app/build.sh
+++ sources/ecommerce-app/build.sh
@@ -7,7 +7,7 @@ echo "Deleting prior build"
 rm -rf ../../static-assets/app/*
 
 echo "Running react build"
-vite build
+yarn build-vite
 
 echo "Restoring .gitkeep"
 touch ../../static-assets/app/.gitkeep
